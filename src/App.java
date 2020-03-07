795dd01bd5d0a899b30838759ca9d01efe37ec5d
friedrw <friedrw@gmail.com>
2020-03-07 12:13:06 -0300

    removendo mensagens estranhas

--- src/App.java
+++ src/App.java
@@ -2,9 +2,6 @@ public class App {
 
     public static void main (String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hou are you?");
-        System.out.println("I'm fine");
-        System.out.println("Thanks");
     }
 
 }
