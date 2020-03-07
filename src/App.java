3dadfcd835ff1dcd756b116ff5743c355d964168
friedrw <friedrw@gmail.com>
2020-03-07 10:57:21 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hou are you?");
+        System.out.println("I'm fine");
+        System.out.println("Thanks");
+    }
+
+}
